e4f0b6a95554a429327933dd813b0283bdd5681b
Milon Prochazka <prochazka@techcrowd.cz>
2024-05-14 23:35:53 +0200

    welcome page

--- src/components/welcome-page/welcome-page.component.ts
+++ src/components/welcome-page/welcome-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrl: './welcome-page.component.css'
+  styleUrl: './welcome-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WelcomePageComponent {
+  numberOfTicks = 0;
 
+  constructor(private ref: ChangeDetectorRef) {
+    setInterval(() => {
+      this.numberOfTicks++;
+      // require view to be updated
+      this.ref.markForCheck();
+    }, 1000);
+  }
+  centered = false;
+  disabled = false;
+  unbounded = false;
+
+  public radius: number = 0;
+  color: string = '#00CED1';
 }
